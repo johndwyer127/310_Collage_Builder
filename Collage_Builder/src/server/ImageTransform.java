1d9a8d8dfc49cb452b8de4d6e5ba57dd734cb89d
suvir-c <scoppara@usc.edu>
2018-02-26 19:13:11 -0800

    image transform class without rotate working - generates A collage

--- Collage_Builder/src/server/ImageTransform.java
+++ Collage_Builder/src/server/ImageTransform.java
@@ -1,9 +1,12 @@
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
@@ -32,9 +35,11 @@ public class ImageTransform {
 	private static final int COLLAGE_SIZE = COLLAGE_WIDTH * COLLAGE_HEIGHT; // total number of pixels
 	private static final int SCALED_IMAGE_SIZE = COLLAGE_SIZE/20;
 //	private static final String GOOGLE_SEARCH_API_KEY = "AIzaSyCQbxRMKMxuyaIVmosCa_k2sIv5BeavGFs";
-	private static final String GOOGLE_SEARCH_API_KEY = "AIzaSyADYi8Ob0jmPJbGEMCkJwrB31bOY80RtXs";
+//	private static final String GOOGLE_SEARCH_API_KEY = "AIzaSyADYi8Ob0jmPJbGEMCkJwrB31bOY80RtXs";
+	private static final String GOOGLE_SEARCH_API_KEY = "AIzaSyAh8tNso-_G-0h5DCft0JibbpPyLYhIPvE";
 //	private static final String GOOGLE_CX = "007628912923159165220:9e6kozm2iea";	// custom search engine identifier
-	private static final String GOOGLE_CX = "008543189839369971484:b8selplq7z8";	// custom search engine identifier
+//	private static final String GOOGLE_CX = "008543189839369971484:b8selplq7z8";	// custom search engine identifier
+	private static final String GOOGLE_CX = "003847142776988134030:4xa5elxfuke";	// custom search engine identifier
 
 	public ImageTransform(String t) {
 		this.topic = t;
@@ -42,27 +47,14 @@ public class ImageTransform {
 	}
 
 	public BufferedImage createCollageImage() {
-		this.fetchImages();
-//		for(BufferedImage image : this.retrievedImages) {
-//			System.out.println("initial height: " + image.getHeight() + ", initial width: " + image.getWidth());
-//		}
-//		for(BufferedImage image : this.retrievedImages) {
-//			System.out.println("bordered height: " + image.getHeight() + ", bordered width: " + image.getWidth());
-//		}
-//		for(BufferedImage image : this.retrievedImages) {
-//			System.out.println("rotated height: " + image.getHeight() + ", rotated width: " + image.getWidth());
-//		}
-		this.resizeImages();
-//		for(BufferedImage image : this.retrievedImages) {
-//			System.out.println("resized height: " + image.getHeight() + ", resized width: " + image.getWidth());
-//		}
-		this.borderImages();
-		this.rotateImages();
-		for(BufferedImage image : this.retrievedImages) {
-			System.out.println("final height: " + image.getHeight() + ", final width: " + image.getWidth());
+		if(this.fetchImages()) {
+			this.resizeImages();
+//			this.rotateImages();
+			this.borderImages();
+			return this.combineImages();
 		}
-		this.combineImages();
-		return null;
+
+		return this.generateInsufficientNumberImage();
 	}
 
 	// retrieves top 30 image search results from Google Custom Search API
@@ -189,32 +181,63 @@ public class ImageTransform {
 
 	// rotating images within IMAGE_ROTATION_LIMIT
 	private void rotateImages() {
-		AffineTransform imageRotator = new AffineTransform();
 		int numImages = this.retrievedImages.size();
-		for(int i = 0; i < numImages; i++) {
-			BufferedImage originalImage = this.retrievedImages.get(0);
-			double rotationAmount = generateRotationAmount();
-			System.out.println("rotationAmount: " + rotationAmount);
+		AffineTransform imageRotator = new AffineTransform();
 
+		for(int i = 0; i < numImages; i++) {
+
+			BufferedImage originalImage = this.retrievedImages.get(0);
+
+
+			int width = originalImage.getWidth();
+			int height = originalImage.getHeight();
+			this.retrievedImages.remove(0);
+
+			double locationX = width/2;
+			double locationY = height/2;
+
+			double rotationAmount = generateRotationAmount();
 			double sin = Math.abs(Math.sin(rotationAmount));
 			double cos = Math.abs(Math.cos(rotationAmount));
 
-			int newWidth = (int) Math.floor(originalImage.getWidth() * cos + originalImage.getHeight() * sin);
-			int newHeight = (int) Math.floor(originalImage.getHeight() * cos + originalImage.getWidth() * sin);
-			System.out.println("oldWidth: " + originalImage.getWidth() + ", originalHeight: " + originalImage.getHeight());
-			System.out.println("newWidth: " + newWidth + ", newHeight: " + newHeight);
-			BufferedImage rotatedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
-			this.retrievedImages.remove(0);
+			double diff = Math.abs(width - height);
+
+			double correctUy = sin;
+			double correctUx = cos;
+
+			if(originalImage.getWidth() < originalImage.getHeight()) {
+				correctUx = sin;
+				correctUy = cos;
+			}
+
+			imageRotator.translate(correctUx*diff, correctUy*diff);
+//			System.out.println("transformationwidth? " + correctUx*diff + ", transformationheight? " + correctUy*diff);
+			imageRotator.rotate(rotationAmount, locationX, locationY);
+
+//			int posAffineTransformOpX = locationX-(int)
+
+//			int newWidth = (int) Math.floor(originalImage.getWidth() * cos + originalImage.getHeight() * sin);
+//			int newHeight = (int) Math.floor(originalImage.getHeight() * cos + originalImage.getWidth() * sin);
+//			System.out.println("oldWidth: " + originalImage.getWidth() + ", originalHeight: " + originalImage.getHeight());
+//			System.out.println("newWidth: " + newWidth + ", newHeight: " + newHeight);
+//			BufferedImage rotatedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+
+			BufferedImage rotatedImageUnscaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+//			BufferedImage rotatedImageUnscaled = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
 
-			imageRotator.rotate(rotationAmount, originalImage.getWidth()/2, originalImage.getHeight()/2);
+//			imageRotator.rotate(rotationAmount, originalImage.getWidth()/2, originalImage.getHeight()/2);
 			AffineTransformOp imageRotatorOp = new AffineTransformOp(imageRotator, AffineTransformOp.TYPE_BILINEAR);
+//			System.out.println("transformedWidth: ? " + width + " , transformedHeight : ? " + height);
+//			rotatedImage = imageRotatorOp.filter(originalImage, null);
+			rotatedImageUnscaled = imageRotatorOp.filter(originalImage, null);
 
-			rotatedImage = imageRotatorOp.filter(originalImage, null);
-
-			this.retrievedImages.add(rotatedImage);
+			this.retrievedImages.add(rotatedImageUnscaled);
 			try {
-				ImageIO.write(rotatedImage,"png",new File(i + "thPicture.png"));
+//				ImageIO.write(rotatedImage,"png",new File(i + "thPictureScaled.png"));
+//				System.out.println("rotatedImageScaledSize: " + rotatedImage.getHeight() + " by " + rotatedImage.getWidth());
+				ImageIO.write(rotatedImageUnscaled, "png", new File(i + "thPictureUnscaled.png"));
+				System.out.println(i + "thRotatedImageUnscaledSize: " + rotatedImageUnscaled.getHeight() + " by " + rotatedImageUnscaled.getWidth());
 			} catch (IOException e) {
 				System.out.println("IO Exception!");
 			}
@@ -227,11 +250,14 @@ public class ImageTransform {
 	// where -45 degrees <= IMAGE_ROTATION_LIMIT <= 45 degrees
 	private double generateRotationAmount() {
 		Random rand = new Random();
-		double angle = rand.nextFloat()*2*Math.PI;
+		double angle = rand.nextFloat();
+		angle *= 2*Math.PI;
 		angle /= 8;
+
 		if(rand.nextBoolean()) {
 			return angle*-1;
 		}
+
 		return angle;
 	}
 
@@ -255,8 +281,8 @@ public class ImageTransform {
 		}
 	}
 
-	// TODO: write method to generate collage from the retrieved bufferedImages
-	private void combineImages() {
+	// generates collage from the retrieved bufferedImages
+	private BufferedImage combineImages() {
 		BufferedImage collageImage = new BufferedImage(COLLAGE_WIDTH, COLLAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		Graphics g = collageImage.getGraphics();
 		int x = 0;
@@ -266,26 +292,39 @@ public class ImageTransform {
 		int colWidth = 0;
 		Random rand = new Random();
 		for(BufferedImage image : this.retrievedImages) {
-			if(x == 0) {
-				rowHeight = image.getHeight();
-			}
-			if(image.getHeight() < rowHeight) {
-				rowHeight = image.getHeight();
-			}
-			if(y+image.getHeight() > COLLAGE_HEIGHT) {
+//			if(x == 0) {
+//				rowHeight = image.getHeight();
+//			}
+//			if(image.getHeight() < rowHeight) {
+//				rowHeight = image.getHeight();
+//			}
+//			if(y+image.getHeight() > COLLAGE_HEIGHT) {
 //				rowHeight = rand.nextInt((COLLAGE_HEIGHT-image.getHeight()) + 1);
-				y = COLLAGE_HEIGHT-image.getHeight();
-			}
-			if(x + image.getWidth() > COLLAGE_WIDTH) {
+////				y = COLLAGE_HEIGHT - image.getHeight();
+//			}
+//			if(x + image.getWidth() > COLLAGE_WIDTH) {
 //				colWidth = rand.nextInt((COLLAGE_WIDTH-image.getWidth()) + 1);
-				x = COLLAGE_WIDTH - image.getWidth();
+////				x = COLLAGE_WIDTH - image.getWidth();
+//			}
+			if (imageNum == 0) {
+				Image tmp = image.getScaledInstance((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, Image.SCALE_SMOOTH);
+				BufferedImage resizedImage = new BufferedImage((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+
+				Graphics2D g2d = resizedImage.createGraphics();
+				g2d.drawImage(tmp, 0, 0, null);
+				g.drawImage(resizedImage, 0, 0, null);
 			}
-			g.drawImage(image, x, y, null);
-			x += image.getWidth();
-			if(x >= COLLAGE_WIDTH) {
-				x = 0;
-				y += rowHeight;
+			else {
+				x = rand.nextInt((COLLAGE_WIDTH-image.getWidth()));
+				y = rand.nextInt((COLLAGE_HEIGHT-image.getHeight()));
+				g.drawImage(image, x, y, null);
+				x += image.getWidth();
+				if(x >= COLLAGE_WIDTH) {
+					x = 0;
+					y += rowHeight;
+				}
 			}
+
 			System.out.println("imageNum: " + imageNum + ", currY: " + y + ", currX: " + x);
 			imageNum++;
 		}
@@ -294,8 +333,43 @@ public class ImageTransform {
 		} catch(IOException e) {
 			e.getMessage();
 		}
-
+		return collageImage;
 	}
+
+	// generates the buffered image that is exported when there is an insufficient number of images found
+	private BufferedImage generateInsufficientNumberImage() {
+		BufferedImage insufficentNumberImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D imageGraphicsManipulator = insufficentNumberImage.createGraphics();
+        Font imageFont = new Font("Arial", Font.PLAIN, 18);
+        imageGraphicsManipulator.setFont(imageFont);
+        imageGraphicsManipulator.dispose();
+
+        insufficentNumberImage = new BufferedImage(COLLAGE_WIDTH, COLLAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        imageGraphicsManipulator = insufficentNumberImage.createGraphics();
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        imageGraphicsManipulator.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        imageGraphicsManipulator.setPaint(Color.WHITE);
+        imageGraphicsManipulator.fillRect(0, 0, COLLAGE_WIDTH, COLLAGE_HEIGHT);
+        imageGraphicsManipulator.setFont(imageFont);
+        imageGraphicsManipulator.setColor(Color.BLACK);
+        imageGraphicsManipulator.drawString("Insufficient number of images found", 445, COLLAGE_HEIGHT/2);
+        imageGraphicsManipulator.dispose();
+
+        try {
+            ImageIO.write(insufficentNumberImage, "png", new File("Text.png"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return insufficentNumberImage;
+
+    }
 
 	public List<BufferedImage> getRetrievedImages() {
 		return this.retrievedImages;
@@ -305,9 +379,4 @@ public class ImageTransform {
 		return this.completeImage;
 	}
 
-	// for testing purposes
-	public static void main(String[] args) {
-		ImageTransform imageTransform = new ImageTransform("dog");
-		imageTransform.createCollageImage();
-	}
 }
